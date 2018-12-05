77e46484b6ace525de08f301c5e6887df98623d2
puneetvashisht@gmail.com <puneetvashsiht@gmail.com>
2018-12-05 11:48:35 +0530

    routing 1

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,50 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Course } from './model/course';
-import { Http } from '@angular/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
 
-  constructor(private http: Http){
+  
 
-  }
-
-  message: string = ''
-
-  ngOnInit(){
-    // perform initialization
-    // this.http.get('assets/dummy.json')
-    console.log('Init...')
-    this.http.get('http://localhost:3000/courses')
-    .toPromise()
-    .then((res)=> res.json())
-    .then((data)=> {
-      console.log(data)
-      this.courses = data;
-    })
-  }
-
-
-  addCourse(title: string, summary: string){
-    console.log(title, summary)
-    var obj = {title, summary}
-    this.http.post('http://localhost:3000/courses', obj)
-    .toPromise() 
-    .then((res)=> res.json())
-    .then((data)=> {
-      console.log(data.message)
-      this.message = data.message
-
-    })
-
-  }
-
-  courses: Array<Course> = [
-
-   
-  ]
 }
